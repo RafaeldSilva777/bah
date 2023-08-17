855824f3191b5120802bc3c67176bb3f6d1ea665
RafaeldSilva <rafaelsilva272005@gmail.com>
2023-08-17 16:25:03 -0300

    prmeiro commit

---
+++ classes/Crud.php
@@ -0,0 +1,74 @@
+<?php
+include_once('conexao/conexao.php');
+
+$db = new Database();
+
+class Crud{
+    private $conn;
+    private $table_name = "carros";
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function create($postValues){
+        $modelo = $postValues['modelo'];
+        $marca = $postValues['marca'];
+        $placa = $postValues['placa'];
+        $cor = $postValues['cor'];
+        $ano = $postValues['ano'];
+
+        $query = "INSERT INTO ". $this->table_name . " (modelo, marca, placa, cor, ano) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $modelo);
+        $stmt->bindParam(2, $marca);
+        $stmt->bindParam(3, $placa);
+        $stmt->bindParam(4, $cor);
+        $stmt->bindParam(5, $ano);
+
+        $rows = $this->read();
+        if($stmt->execute()){
+            print "<script>alert('Cadastro OK!')</script>";
+            print "<script> location.href='?action=read'; </script>";
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public function read(){
+        $query = "SELECT * FROM ". $this->table_name;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    //funcao atualizar registro
+public function update($postValues) {
+    $id = $postValues['id'];
+    $modelo = $postValues['modelo'];
+    $marca = $postValues['marca'];
+    $placa = $postValues['placa'];
+    $cor = $postValues['cor'];
+    $ano = $postValues['ano'];
+
+    if(empty($id) || empty($modelo) || empty($marca) || empty($placa) || empty($cor) || empty($ano)) {
+    return false;
+    }
+
+    $query = "UPDATE" . $this->table_name . " SET  modelo = ?, SET  marca = ?, SET placa = ?, SET  cor = ?, SET ano = ? WHERE id = ?";
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(1, $modelo);
+    $stmt->bindParam(2, $marca);
+    $stmt->bindParam(3, $placa);
+    $stmt->bindParam(4, $cor);
+    $stmt->bindParam(5, $ano);
+    $stmt->bindParam(6, $id);
+    if($stmt->execute()){
+    return true;
+    } else {
+    return false;
+    }
+}
+}
+
+?>
